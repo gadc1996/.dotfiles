59f87fbd0448d426d0ff413ea0de665b837f88ec
Alex Delgado <gadc1996@gmail.com>
2021-02-17 06:18:37 -0700

    Add fish promt Add i3wm Move git config to organize by folders

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,3 +1,5 @@
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.config
+ln -s ~/.dotfiles/i3/config ~/.config/i3/config
+ln -s ~/.dotfiles/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
